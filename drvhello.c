1d0c569c23ac721b00137b5df8959d4be7f4b123
Johannes Natter <office@dsp-crowd.com>
2017-04-27 20:11:57 +0000

    implement init and exit function

--- drvhello.c
+++ drvhello.c
@@ -1,5 +1,21 @@
 #include <linux/module.h>
 
+static int drvhello_init(void) {
+
+	printk("%s: Entry\n", __func__);
+
+	return 0;
+}
+
+static void drvhello_exit(void) {
+
+	printk("%s: Done\n", __func__);
+
+}
+
+module_init(drvhello_init);
+module_exit(drvhello_exit);
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Johannes Natter");
 MODULE_DESCRIPTION("Linux driver hello world");
